946a8ced1536236c7fa4eade3f39b80942fb368e
thaidd7b <thaidd7b@gmail.com>
2022-09-29 00:49:16 +0700

    save

--- test/unitTest.js
+++ test/unitTest.js
@@ -3,11 +3,11 @@ const { seedInput } = require("../createInput");
 var expect = require("chai").expect;
 
 const input = seedInput(100, 100);
-// const result = sortTest(100, 100, input)
+const result = sortTest(100, 100, input)
 
 describe("Ket qua", function () {
-    it("Array B1", function () {
-      expect(array.length, "Số lượng phần tử phải lớn hơn 3").to.greaterThan(3);
+    it("sort 100 * 100", function () {
+        expect(result.toString(), "Số lượng phần tử phải lớn hơn 3").to.equal(input.toString().split(',').sort().toString());
     });
 })
 
